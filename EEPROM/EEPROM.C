f1f37537e07ea97ed0528e89f31f854a7891f9a5
abd elwahed dardeer <89838156+Eldardeery@users.noreply.github.com>
2021-10-04 23:18:57 +0200

    Update EEPROM.C

--- EEPROM/EEPROM.C
+++ EEPROM/EEPROM.C
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 
@@ -9,8 +8,9 @@ void EEprom_Init()
 {
 
 }
-void EEprom_Write(uint8 address, uint8 data)
+void EEprom_Write(uint16 address, uint8 data)
 {
+	
 	EEARL=(unsigned char)address;
 	EEARH=(unsigned char)address>>8;
 	EEDR=data;
@@ -28,54 +28,15 @@ uint8 EEprom_Read(uint8 address)
 }
 
 
-void EEprom_Write_Data(uint8 Address, uint8 data)
+void EEprom_Write_Data(uint16 Address, uint8 data[])
 {
 	uint8 i =0 ;
-	while(data[i]!=NULL)
+	while(data[i]!=0)
 	{
 		EEprom_Write(Address,data[i]);
 		Address++;
 		i++;
 	}
 }
-=======
 
 
-
-#include "EEPROM.H"
-
-
-void EEprom_Init()
-{
-
-}
-void EEprom_Write(uint8 address, uint8 data)
-{
-	EEARL=(unsigned char)address;
-	EEARH=(unsigned char)address>>8;
-	EEDR=data;
-	EECR|=(1<<EEMWE);
-	EECR|=(1<<EEWE);
-	while(((EECR&(1<<EEWE))>>EEWE)==1);
-
-}
-uint8 EEprom_Read(uint8 address)
-{
-	EEARL=(unsigned char)address;
-	EEARH=(unsigned char)address>>8;
-	EECR|=(1<<EERE);
-	return EEDR;
-}
-
-
-void EEprom_Write_Data(uint8 Address, uint8 data)
-{
-	uint8 i =0 ;
-	while(data[i]!=NULL)
-	{
-		EEprom_Write(Address,data[i]);
-		Address++;
-		i++;
-	}
-}
->>>>>>> 50770696eabc6891b153c0666b8d9d5b1191c063
